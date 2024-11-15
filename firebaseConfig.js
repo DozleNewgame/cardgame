eaa1a699abbb5ed525fb027b9f753764c601b64c
DozleNewgame <lynch@dozle.jp>
2024-11-15 20:05:18 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 // Firebaseの設定情報
 const firebaseConfig = {
-    apiKey: "AIzaSyDFGw8AJ9_v_zk0p8iyxeep6rwQrMG2I",
+    apiKey: "AIzaSyDFFgw8AyJ9_v_zk0p0iyxeep6rwQrmG2I",
     authDomain: "dozlecard-28e2d.firebaseapp.com",
     databaseURL: "https://dozlecard-28e2d-default-rtdb.firebaseio.com",
     projectId: "dozlecard-28e2d",
-    storageBucket: "dozlecard-28e2d.appspot.com",
-    messagingSenderId: "767591771764",
-    appId: "1:767591771764:web:a4bb44566eabc34b06d021",
-    measurementId: "G-HY8R46SGFM"
+    storageBucket: "dozlecard-28e2d.firebasestorage.app",
+    messagingSenderId: "678591771764",
+    appId: "1:678591771764:web:a40b44566eabc34b060d21",
+    measurementId: "G-HY8R4C5GFM"
 };
 
 // Firebaseの初期化
